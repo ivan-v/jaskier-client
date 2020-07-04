9ced6496b0d012d6f72a5128b55331c5455e08ef
Jeff Schwab <jeff@unbuggy.com>
2020-07-04 19:21:42 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+function main() {
+  const a = document.createElement('a');
+  a.append('Generate');
+  a.href = 'https://modern-bard.uk.r.appspot.com/';
+  a.classList.add('midi-link');
+
+  document.body.append(a);
+}
+
+main();
