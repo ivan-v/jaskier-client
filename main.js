d6adc81bf1e1e39bc931d59a8a0952fd1debdec2
ivan-v <navioriv@gmail.com>
2020-07-07 12:52:46 -0400

    Add play/pause button, change "generate" button to say "download"

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 function main() {
   const a = document.createElement('a');
-  a.append('Generate');
+  a.append('Download');
   //a.href = 'https://modern-bard.uk.r.appspot.com/';
   a.href = 'http://localhost:8013/';
   a.classList.add('midi-link');
 
   document.body.append(a);
 }
+
 
 main();
