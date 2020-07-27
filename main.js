334b5f7a478a1c2ced85d7c6d2331bda19a9724d
ivan-v <navioriv@gmail.com>
2020-07-27 14:15:51 -0400

    Fixed submit_backing_gen event listner

--- main.js
+++ main.js
@@ -139,7 +139,7 @@ function main() {
         });
     } else {
         const submit_backing_gen = document.getElementById("submit_backing_gen")
-        submit_song_gen.addEventListener("click", (event) => {
+        submit_backing_gen.addEventListener("click", (event) => {
             
             console.log('submitting backing gen!')
             
